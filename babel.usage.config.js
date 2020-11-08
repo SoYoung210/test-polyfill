06946bf1410470f02b4726971f6f908eb4b7278c
soso <ethdud1@gmail.com>
2020-11-08 21:32:50 +0900

    feat: add babel usage config example

---
+++ babel.usage.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "79",
+          ie: "11", // ie 11까지 지원하는 코드를 만든다
+        },
+        useBuiltIns: "usage",
+        corejs: 3
+      },
+    ],
+  ],
+}
