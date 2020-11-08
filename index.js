3d348a6afa27361e614027c2b014d8c33edf905b
soso <ethdud1@gmail.com>
2020-11-08 17:25:04 +0900

    feat: add babel-transform-runtime example

---
+++ index.js
@@ -0,0 +1,7 @@
+// Test Promise
+// it uses "babel-runtime/core-js/promise"
+new Promise(resolve => resolve(1))
+// Test Class "babel-runtime/helpers/classCallCheck"
+class Person {
+}
+const set1 = new Set([1, 2, 3, 4, 5]);
